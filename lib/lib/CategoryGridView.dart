19e5ab735b2ebe45358f74d5957b9f2076841784
Vu Pham <cr.vanvu@gmail.com>
2022-03-31 01:22:44 +0700

    create CategoryGridView.dart

---
+++ lib/lib/CategoryGridView.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'CategoryV.dart';
+
+import 'Category.dart';
+
+class CategoryGridView extends StatelessWidget {
+  Future<List<Category>> categories;
+
+  CategoryGridView({Key? key, required this.categories}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.zero,
+      child: FutureBuilder<List<Category>>(
+        future: categories,
+        builder: (context, snapshot) {
+          if (snapshot.hasData){
+            return GridView.builder(
+                itemCount: snapshot.data?.length,
+                scrollDirection: Axis.vertical,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    child: CategoryV(category: snapshot.data![index],),
+                    onTap: () {
+
+                    },
+                  );
+                });
+          } else if (snapshot.hasError){
+            return Text("Error");
+          } else {
+            return Text("Loading...");
+          }
+
+        },
+      )
+    );
+  }
+}
