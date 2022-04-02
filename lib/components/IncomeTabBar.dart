077b290c9ba5d13c0a24c476126417dde5acfa94
daotung8801 <daoxuantung8801@gmail.com>
2022-04-02 16:09:07 +0700

    Add body tabbar component

---
+++ lib/components/IncomeTabBar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'DatePicker.dart';
+import 'DateRangePicker.dart';
+import 'TitleText1.dart';
+
+class IncomeTabBar extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return DefaultTabController(
+      length: 5, // length of tabs
+      child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
+        Container(
+          child: TabBar(
+            labelColor: Color.fromARGB(255, 35, 111, 87),
+            labelStyle: TextStyle(fontSize: 15, fontFamily: 'Inter', fontWeight: FontWeight.bold),
+            unselectedLabelColor: Colors.black,
+            indicatorWeight: 2.2,
+            indicatorColor: Color.fromARGB(255, 35, 111, 87),
+            padding: EdgeInsets.only(top: 10),
+            indicatorPadding: EdgeInsets.only(left: 25.0, bottom: 5.0, right: 0.0),
+            labelPadding: EdgeInsets.only(left: 25),
+            isScrollable: true,
+            tabs: [
+              Tab(text: 'Ngày'),
+              Tab(text: 'Tuần'),
+              Tab(text: 'Tháng'),
+              Tab(text: 'Năm'),
+              Tab(text: 'Khoảng thời gian'),
+            ],
+          ),
+        ),
+        Container(
+          height: 400, //height of TabBarView
+          decoration: BoxDecoration(
+          ),
+          child: TabBarView(
+              children: <Widget>[
+                Center(
+                  child: Column(
+                    children: <Widget>[
+                      DatePicker(),
+                    ],
+                  ),
+                ),
+                Center(child: TitleText1(text: 'Tab 2', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
+                Center(child: TitleText1(text: 'Tab 3', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
+                Center(child: TitleText1(text: 'Tab 4', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
+                Center(
+                  child: Column(
+                    children: <Widget>[
+                      DateRangePicker(),
+                    ],
+                  ),
+                ),
+              ]
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/screens/ExchangeMoney.dart
+++ lib/screens/ExchangeMoney.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../components/DateRangePicker.dart';
-import '../components/DatePicker.dart';
+import '../components/ExpenseTabBar.dart';
+import '../components/IncomeTabBar.dart';
 import '../components/PopUpNotification1.dart';
 import '../components/SearchItem.dart';
 import '../components/TitleText1.dart';
@@ -96,59 +96,11 @@ class ExchangeMoney extends StatelessWidget {
         body: TabBarView(
           children: [
             Center(
-              child: DefaultTabController(
-                length: 5, // length of tabs
-                child: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: <Widget>[
-                  Container(
-                    child: TabBar(
-                      labelColor: Color.fromARGB(255, 35, 111, 87),
-                      labelStyle: TextStyle(fontSize: 15, fontFamily: 'Inter', fontWeight: FontWeight.bold),
-                      unselectedLabelColor: Colors.black,
-                      indicatorWeight: 2.2,
-                      indicatorColor: Color.fromARGB(255, 35, 111, 87),
-                      padding: EdgeInsets.only(top: 10),
-                      indicatorPadding: EdgeInsets.only(left: 25.0, bottom: 5.0, right: 0.0),
-                      labelPadding: EdgeInsets.only(left: 25),
-                      isScrollable: true,
-                      tabs: [
-                        Tab(text: 'Ngày'),
-                        Tab(text: 'Tuần'),
-                        Tab(text: 'Tháng'),
-                        Tab(text: 'Năm'),
-                        Tab(text: 'Khoảng thời gian'),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    height: 400, //height of TabBarView
-                    decoration: BoxDecoration(
-                    ),
-                    child: TabBarView(
-                      children: <Widget>[
-                        Center(
-                          child: Column(
-                            children: <Widget>[
-                              DatePicker(),
-                            ],
-                          ),
-                        ),
-                        Center(child: TitleText1(text: 'Tab 2', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
-                        Center(child: TitleText1(text: 'Tab 3', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
-                        Center(child: TitleText1(text: 'Tab 4', fontFamily: 'Inter', fontSize: 25, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
-                        Center(
-                          child: Column(
-                            children: <Widget>[
-                              DateRangePicker(),
-                            ],
-                          ),
-                        ),
-                      ]
-                    ),
-                  ),
-                ]),
-              ),
+              child: ExpenseTabBar(),
             ),
-            Center(child: TitleText1(text: 'Thu nhập', fontFamily: 'Inter', fontSize: 18, fontWeight: FontWeight.normal, r: 0, g: 0, b: 0)),
+            Center(
+              child: IncomeTabBar(),
+            ),
           ],
         ),
       ),
