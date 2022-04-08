df7e43a81514f6c940739edddf9db0c2b63bcc11
kieuvantuyen01 <tuyenkieuvan@gmail.com>
2022-04-08 15:18:09 +0700

    fix align text left at Contact screen

--- lib/screen/Contact.dart
+++ lib/screen/Contact.dart
@@ -11,34 +11,37 @@ class Contact extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      appBar: SmallHeader(title: 'Nhắc nhở', icon: Icon(Icons.menu)),
+      appBar: SmallHeader(title: 'Liên hệ', icon: Icon(Icons.menu)),
       body: Column(
         children: <Widget>[
           Padding(
             padding: EdgeInsets.only(left: 16, right: 16, top: 60),
             child:
-                InputText1(hintText: 'Chủ đề thư', labelText: '', maxLines: 1),
+            InputText1(hintText: '', labelText: 'Chủ đề thư', maxLines: 1),
           ),
           Padding(
             padding: EdgeInsets.only(left: 16, right: 16, top: 20),
             child: InputText1(
-                hintText: 'Địa chỉ email', labelText: '', maxLines: 1),
+                hintText: '', labelText: 'Địa chỉ email', maxLines: 1),
           ),
           Padding(
-            padding: EdgeInsets.only(left: 16, right: 16, top: 5),
-            child: TitleText1(
-                text: 'Thư trả lời sẽ được gửi đến địa chỉ email này',
-                fontFamily: 'Inter',
-                fontSize: 16,
-                fontWeight: FontWeight.normal,
-                r: 0,
-                g: 0,
-                b: 0),
+              padding: EdgeInsets.only(left: 16, right: 16, top: 5),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: TitleText1(
+                    text: 'Thư trả lời sẽ được gửi đến địa chỉ email này',
+                    fontFamily: 'Inter',
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    r: 0,
+                    g: 0,
+                    b: 0),
+              )
           ),
           Padding(
             padding: EdgeInsets.only(left: 16, right: 16, top: 20),
             child: InputText1(
-                hintText: 'Nội dung thư', labelText: '', maxLines: 1),
+                hintText: '', labelText: 'Nội dung thư', maxLines: 1),
           ),
           Padding(
             padding: EdgeInsets.only(top: 90),
